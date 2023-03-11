464cd54c536d67a2d5f45d521ebbf17be146a833
jisooround <9utesleben@gmail.com>
2023-03-11 22:21:22 +0900

    Feat: 이벤트함수 추가 &  클릭시 해당 게시글 데이터 응답 & title 노출

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const container = document.getElementById("root");
 const ajax = new XMLHttpRequest();
+const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
 ajax.open("GET", NEWS_URL, false); //동기 처리 방식
 ajax.send();
@@ -7,11 +10,29 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement("ul");
 
+window.addEventListener("hashchange", () => {
+  const id = location.hash.substring(1);
+
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement("h1");
+
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+});
+
 for (let i = 0; i < 10; i++) {
   const li = document.createElement("li");
+  const a = document.createElement("a");
 
-  li.innerHTML = `<li>${newsFeed[i].title}</li>`;
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `<li>${newsFeed[i].title}</li> (${newsFeed[i].comments_count})`;
+
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById("root").appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
